c84659fc95e92a98cb6d32aa42e82c58831989e2
xiaozhong <3309431527@qq.com>
2020-03-06 16:49:40 +0800

    添加了一个add方法

---
+++ test_git/src/com/zcm/test/TestDemo.java
@@ -0,0 +1,13 @@
+package com.zcm.test;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static int  add(int i,int j){
+        int total ;
+        total = i+j;
+        return total;
+    }
+}
