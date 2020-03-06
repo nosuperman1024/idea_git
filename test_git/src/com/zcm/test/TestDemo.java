2cf753e91b97f31574980009bf3785466c7b1d03
xiaozhong <3309431527@qq.com>
2020-03-06 16:51:27 +0800

    添加了两个变量

--- test_git/src/com/zcm/test/TestDemo.java
+++ test_git/src/com/zcm/test/TestDemo.java
@@ -3,6 +3,9 @@ package com.zcm.test;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int i;
+        int j;
+
     }
 
     public static int  add(int i,int j){
